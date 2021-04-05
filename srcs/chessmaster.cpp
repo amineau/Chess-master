b33c203007bfd4f795aec14da2164a8a8366d0cb
gitlab@mineau.fr <antoine@bimdata.io>
2021-04-05 16:59:39 +0200

    use real default fen start

--- srcs/chessmaster.cpp
+++ srcs/chessmaster.cpp
@@ -6,14 +6,13 @@
 /*   By: amineau <antoine@mineau.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 13:36:58 by amineau           #+#    #+#             */
-/*   Updated: 2021/01/20 00:20:15 by amineau          ###   ########.fr       */
+/*   Updated: 2021/04/05 16:59:13 by amineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "chessmaster.hpp"
 
 namespace settings {
-const std::string defaultFenStart = "4k3/8/8/r2Pp2K/8/8/8/8 w - e6 0 1";
-// const std::string defaultFenStart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+const std::string defaultFenStart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 const std::string defaultInterface = "cli";
 }
